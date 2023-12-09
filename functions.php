127dffb1a93b5044c0bcc3dd4f8c4c5a57741589
FlavioAraujo23 <flaviolopes1027@gmail.com>
2023-12-09 10:22:18 -0300

    :bug: Fix: Adicionei um retorno que estava faltando no functions.php

--- functions.php
+++ functions.php
@@ -2,10 +2,11 @@
 // Remove as rotas definidas pelo WordPress
 // remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
-add_filter('rest_endpoints' . function($endpoints) {
-  unset($endpoints['/wp/v1/users']);
-  unset($endpoints['/wp/v1/users/(?P<id>[0-9]+)']);
-}); 
+add_filter('rest_endpoints', function ($endpoints) {
+  unset($endpoints['/wp/v2/users']);
+  unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+  return $endpoints;
+});
 
 $dirbase = get_template_directory();
 
